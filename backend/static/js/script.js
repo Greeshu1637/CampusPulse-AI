5d83bac11852b8b3a75abca396679d875723100f
Greeshma Polanki <vu.241fa04776@gmail.com>
2026-07-21 19:27:36 +0530

    Added live Chart.js dashboard using PostgreSQL data

--- backend/static/js/script.js
+++ backend/static/js/script.js
@@ -1,3 +1,41 @@
-function sayHello(){
-    alert("Welcome to CampusPulse AI 🚀");
+const ctx = document.getElementById('complaintChart');
+
+if (ctx) {
+
+    new Chart(ctx, {
+
+        type: 'pie',
+
+        data: {
+
+            labels: ['Pending', 'Resolved'],
+
+            datasets: [{
+
+                data: [pendingComplaints, resolvedComplaints],
+
+                borderWidth: 1
+
+            }]
+
+        },
+
+        options: {
+
+            responsive: true,
+
+            plugins: {
+
+                legend: {
+
+                    position: 'bottom'
+
+                }
+
+            }
+
+        }
+
+    });
+
 }
